89b772f97096224c496632a4e5819b20ca185a75
Towhidul Islam <m.towhid.islam@gmail.com>
2022-11-23 18:24:31 +0600

    payment:impl: adding

---
+++ PaymentService/src/main/java/com/infoworks/lab/webapp/config/BeanConfig.java
@@ -0,0 +1,41 @@
+package com.infoworks.lab.webapp.config;
+
+import com.infoworks.lab.util.services.iResourceService;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+@Configuration
+public class BeanConfig {
+
+    private Environment env;
+
+    public BeanConfig(@Autowired Environment env) {
+        this.env = env;
+    }
+
+    @Bean
+    RedissonClient getRedisClient(){
+        String redisHost = env.getProperty("app.redis.host") != null
+                ? env.getProperty("app.redis.host") : "localhost";
+        String redisPort = env.getProperty("app.redis.port") != null
+                ? env.getProperty("app.redis.port") : "6379";
+        Config conf = new Config();
+        conf.useSingleServer()
+                .setAddress(String.format("redis://%s:%s",redisHost, redisPort))
+                .setRetryAttempts(5)
+                .setRetryInterval(1500);
+        //RedissionClient instance are fully-thread safe.
+        return Redisson.create(conf);
+    }
+
+    @Bean
+    public iResourceService getResourceService(){
+        return iResourceService.create();
+    }
+
+}
