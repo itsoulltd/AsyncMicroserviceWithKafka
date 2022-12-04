d325893a8e175f5d987b23da5611c2d43cc2a8ad
Towhidul Islam <m.towhid.islam@gmail.com>
2022-12-04 16:21:48 +0600

    product: adding async purchase order.

--- ProductService/src/main/java/com/infoworks/lab/controllers/rest/ProductController.java
+++ ProductService/src/main/java/com/infoworks/lab/controllers/rest/ProductController.java
@@ -1,15 +1,17 @@
 package com.infoworks.lab.controllers.rest;
 
 import com.infoworks.lab.beans.tasks.definition.TaskQueue;
-import com.infoworks.lab.domain.tasks.ConsolePrintTask;
-import com.infoworks.lab.rest.models.Message;
+import com.infoworks.lab.domain.tasks.PurchaseTask;
 import com.infoworks.lab.rest.models.Response;
 import com.infoworks.lab.rest.models.SearchQuery;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
@@ -23,23 +25,26 @@ public class ProductController {
         this.queue = queue;
     }
 
-    @GetMapping("/print/{message}")
-    public ResponseEntity<String> print(@PathVariable("message") final String message){
-        //Type-1:DispatchTaskInto-KafkaQueue:-
-        ConsolePrintTask task = new ConsolePrintTask();
-        task.setMessage(new Message().setPayload("ProductService" + message));
+    @PostMapping("/purchase/async")
+    public ResponseEntity<String> asyncPurchase(@RequestBody SearchQuery purchase){
+        LOG.info("ASYNC PURCHASE: Message received {} ", purchase.toString());
+        //Adding additional attribute:
+        purchase.add("order-id").isEqualTo("01928373");
+        //Type-1:Dispatch PurchaseTask Into KafkaQueue:-
+        PurchaseTask task = new PurchaseTask(purchase);
         queue.add(task);
-        return new ResponseEntity(message, HttpStatus.OK);
+        return new ResponseEntity("Async Purchase Has Been Dispatched", HttpStatus.OK);
     }
 
     @PostMapping("/purchase")
     public ResponseEntity<Response> purchase(@RequestBody SearchQuery purchase) {
         LOG.info("PURCHASE: Message received {} ", purchase.toString());
+        //Adding additional attribute:
+        purchase.add("order-id").isEqualTo("01928374");
         //Call RestTemplate: api/payment/v1/checkout
         RestTemplate template = new RestTemplateBuilder()
                 .rootUri("http://localhost:8092/api/payment")
                 .build();
-        purchase.add("order-id").isEqualTo("01928374");
         HttpEntity<SearchQuery> entity = new HttpEntity<>(purchase, new HttpHeaders());
         ResponseEntity<Response> res = template.exchange("/v1/checkout"
                 , HttpMethod.POST
---
+++ ProductService/src/main/java/com/infoworks/lab/domain/tasks/PurchaseTask.java
@@ -0,0 +1,33 @@
+package com.infoworks.lab.domain.tasks;
+
+import com.infoworks.lab.beans.tasks.nuts.ExecutableTask;
+import com.infoworks.lab.rest.models.Message;
+import com.infoworks.lab.rest.models.Response;
+import com.infoworks.lab.rest.models.SearchQuery;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
+
+public class PurchaseTask extends ExecutableTask<Message, Response> {
+
+    public PurchaseTask() {}
+
+    public PurchaseTask(SearchQuery query) {
+        getMessage().setEvent(query);
+    }
+
+    @Override
+    public Response execute(Message message) throws RuntimeException {
+        RestTemplate template = new RestTemplateBuilder()
+                .rootUri("http://localhost:8092/api/payment")
+                .build();
+        SearchQuery purchase = (SearchQuery) getMessage().getEvent(SearchQuery.class);
+        HttpEntity<SearchQuery> entity = new HttpEntity<>(purchase, new HttpHeaders());
+        ResponseEntity<Response> res = template.exchange("/v1/checkout"
+                , HttpMethod.POST
+                , entity
+                , Response.class);
+        return res.getBody();
+    }
+
+}
