d3f63eec5b906d35505917461e1313476db3779d
DEGRUYTER\towhid.islam <towhid.islam@degruyterbrill.com>
2026-07-03 13:35:01 +0200

    updated

---
+++ DeliveryDispatchService/src/main/java/com.infoworks.lab/domain/queue/TaskDispatchQueue.java
@@ -0,0 +1,63 @@
+package com.infoworks.lab.domain.queue;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.infoworks.objects.Message;
+import com.infoworks.tasks.Task;
+import com.infoworks.tasks.queue.TaskQueue;
+import com.infoworks.utils.jmsq.AbstractJmsQueue;
+import com.infoworks.utils.jmsq.JmsMessage;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component("taskDispatchQueue")
+public class TaskDispatchQueue extends AbstractJmsQueue {
+
+    private KafkaTemplate<String, String> kafkaTemplate;
+    private ObjectMapper objectMapper;
+
+    public TaskDispatchQueue(@Qualifier("kafkaTextTemplate") KafkaTemplate kafkaTemplate
+            , ObjectMapper objectMapper) {
+        this.kafkaTemplate = kafkaTemplate;
+        this.objectMapper = objectMapper;
+    }
+
+    @Value("${topic.delivery.execute}")
+    private String exeQueue;
+
+    @Value("${topic.delivery.abort}")
+    private String abortQueue;
+
+    @Override
+    public TaskQueue add(Task task) {
+        //Defined:JmsMessage Protocol
+        JmsMessage jmsMessage = convert(task);
+        kafkaTemplate.send(exeQueue, jmsMessage.toString());
+        return this;
+    }
+
+    @Override
+    public void abort(Task task, Message error) {
+        //Defined:JmsMessage Protocol
+        JmsMessage jmsMessage = convert(task, error);
+        kafkaTemplate.send(abortQueue, jmsMessage.toString());
+    }
+
+    @Override
+    public TaskQueue cancel(Task task) {
+        //TODO:
+        return this;
+    }
+
+    @Override
+    public ObjectMapper getObjectMapper() {
+        return objectMapper;
+    }
+
+    @Override
+    public void setObjectMapper(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+}
